032695f4b0030f2e86801b4caaf98e1f777f8962
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2023-08-27 23:49:18 -0300

    new parameters

--- Proj/fkIndex.py
+++ Proj/fkIndex.py
@@ -1,32 +1,38 @@
 import streamlit as st
 from main import *
 
-TITLE = "OCEV - Computação Evolucionária"
-SUBTITLE = "Ferramenta Dedicada a aplicações solicitadas na disciplina de Computação Evolucionária da UDESC"
+TITLE = "OCEV - Computação Evolutiva - Trabalho 2023/2"
+
 if __name__ == "__main__":
     #Header
     st.set_page_config(TITLE, layout="wide")
-    st.divider()
-    st.title(TITLE)
-    st.subheader(SUBTITLE)
-    st.caption("Feito por Bruno Marchi Pires")
-    st.divider()
-    
+    st.header(TITLE, divider='rainbow')
+   
     #Select Problem
-    problems=["Nenhum","N-Queens",]
-    selectedProblem = st.selectbox("Selecione o Problema para Atacar:", problems)
+    problemsList=["Nenhum","N-Queens",]
+    selectedProblem = st.selectbox("Selecione o Problema para Atacar:", problemsList)
     print(selectedProblem)
 
     #Select Individual Type
+    individualsTypeList=["Inteiro", "Inteiro Permutado", "Binário", "Real"]
+    individualType = st.selectbox("Selecione a Codificação (Tipo) dos Indivíduos:", individualsTypeList)
+    print(individualType)
 
     #Select Individual Dimension
+    dimension = st.slider("Selecione a Dimensão dos Indivíduos:")
+    print(dimension)
 
     #Select Population Size
+    populationSize = st.slider("Selecione o Tamanho da População Inicial:")
+    print(populationSize)
+    
+    #Executa
+    execute = st.button("ATTACK!", use_container_width=True)
 
-    individual_type, dimension, population_size, problem = ler_arquivo_json()
-    createdPopulation = create_pop(individual_type, population_size, dimension)
-    createdPopulation = apply_problem(createdPopulation, problem)
-    printPopulation(createdPopulation)
+    #individual_type, dimension, population_size, selectedProblem = ler_arquivo_json()
+    #createdPopulation = create_pop(individual_type, population_size, dimension)
+    #createdPopulation = apply_problem(createdPopulation, problem)
+    #printPopulation(createdPopulation)
 
 
 
