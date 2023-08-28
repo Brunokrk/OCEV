b366f310d3db24e9a6f2e6867e2aa45b8abb7b2b
Brunokrk <piresbr@outlook.com>
2023-08-28 14:31:51 -0300

    whiteSpaces

--- Proj/fkIndex.py
+++ Proj/fkIndex.py
@@ -8,25 +8,16 @@ if __name__ == "__main__":
     #Header
     st.set_page_config(TITLE, layout="wide")
     st.header(TITLE, divider='rainbow')
-   
     #Select Problem
     problemsList=["Nenhum","N-Queens",]
     selectedProblem = st.selectbox("Selecione o Problema para Atacar:", problemsList)
-    #print(selectedProblem)
-
     #Select Individual Type
     individualsTypeList=["Inteiro", "Inteiro Permutado", "Binário", "Real"]
     individualType = st.selectbox("Selecione a Codificação (Tipo) dos Indivíduos:", individualsTypeList)
-    #print(individualType)
-
     #Select Individual Dimension
     dimension = st.slider("Selecione a Dimensão dos Indivíduos:")
-    #print(dimension)
-
     #Select Population Size
     populationSize = st.slider("Selecione o Tamanho da População Inicial:")
-    #print(populationSize)
-    
     #Executa
     executeAttack = st.button("ATTACK!", use_container_width=True)
     if executeAttack:
@@ -41,6 +32,8 @@ if __name__ == "__main__":
             st.header("Score", divider = True)
             for idx, individual in enumerate(createdPopulation):
                 st.write(f"Individual {idx+1}: {individual.score}")
+        
+        
     
     #individual_type, dimension, population_size, selectedProblem = ler_arquivo_json()
     #createdPopulation = create_pop(individual_type, population_size, dimension)
